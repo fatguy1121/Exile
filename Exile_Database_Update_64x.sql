7011689f36dfb5c544d03b3da50e866ec633a2e7
Brett.Nordin <Brett.Nordin@live.ca>
2017-03-22 20:30:29 -0600

    Main Push
    Moved all Files

--- Exile_Database_Update_64x.sql
+++ Exile_Database_Update_64x.sql
@@ -4,3 +4,4 @@ UPDATE (clan) SET name = REPLACE(name, '"', '');
 UPDATE (account) SET name = REPLACE(name, '"', '');
 UPDATE (clan_map_marker) SET label = REPLACE(label, '"', '');
 UPDATE (territory) SET name = REPLACE(name, '"', '');
+ALTER TABLE `player` CHANGE COLUMN `hitpoints` `hitpoints` VARCHAR(1024) NOT NULL DEFAULT '[]' ;
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Exile
+In your config.cpp 
+Add: #include "Config\CfgExileCustomCode.cpp" 
+Into:
+class CfgExileCustomCode 
+{
+};
+Drag and drop files into mission folder
